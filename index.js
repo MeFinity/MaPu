1cb6236e2b27475ca6fc1fc5975b3c3bb10e7ac4
MeFinity <72972952+MeFinity@users.noreply.github.com>
2023-03-05 14:56:26 +0700

    it infinity now

--- index.js
+++ index.js
@@ -6,35 +6,56 @@ function loader() {
   var data = JSON.parse(save);
   if (data.money !== undefined) {
     var antimatter = document.getElementById("d-money");
-    antimatter.value = data.money;
+    antimatter.value = new Decimal(data.money).toString();
   }
   if (data.totalmoney !== undefined) {
     var totalantimatter = document.getElementById("d-totalmoney");
-    totalantimatter.value = data.totalmoney;
+    totalantimatter.value = new Decimal(data.totalmoney).toString();
   }
   if (data.tickspeed !== undefined) {
     var tickspeed = document.getElementById("d-tickspeed");
-    tickspeed.value = data.tickspeed;
+    tickspeed.value = new Decimal(data.tickspeed).toString();
   }
   if (data.tickSpeedCost !== undefined) {
     var tickspeedcost = document.getElementById("d-tickspeedcost");
-    tickspeedcost.value = data.tickSpeedCost;
+    tickspeedcost.value = new Decimal(data.tickSpeedCost).toString();
+  }
+  if (data.tickspeedMultiplier !== undefined) {
+    var tickspeedMultiplier = document.getElementById("d-tickspeedmultiplier");
+    tickspeedMultiplier.value = new Decimal(data.tickspeedMultiplier).toString();
+  }
+  if (data.tickSpeedMultDecrease !== undefined) {
+    var tickSpeedMultDecrease = document.getElementById("d-tickspeedmultdecrease");
+    tickSpeedMultDecrease.value = new Decimal(data.tickSpeedMultDecrease).toString();
+  }
+  if (data.tickSpeedMultDecreaseCost !== undefined) {
+    var tickSpeedMultDecreaseCost = document.getElementById("d-tickspeedmultdecreasecost");
+    tickSpeedMultDecreaseCost.value = new Decimal(data.tickSpeedMultDecreaseCost).toString();
   }
 }
 
 function saver() {
   var updatedData = JSON.parse(document.getElementById("display").value);
   if (updatedData.money !== undefined) {
-    updatedData.money = document.getElementById("d-money").value;
+    updatedData.money = new Decimal(document.getElementById("d-money").value).toString();
   }
   if (updatedData.totalmoney !== undefined) {
-    updatedData.totalmoney = document.getElementById("d-totalmoney").value;
+    updatedData.totalmoney = new Decimal(document.getElementById("d-totalmoney").value).toString();
   }
   if (updatedData.tickspeed !== undefined) {
-    updatedData.tickspeed = document.getElementById("d-tickspeed").value;
+    updatedData.tickspeed = new Decimal(document.getElementById("d-tickspeed").value).toString();
   }
   if (updatedData.tickSpeedCost !== undefined) {
-    updatedData.tickSpeedCost = document.getElementById("d-tickspeedcost").value;
+    updatedData.tickSpeedCost = new Decimal(document.getElementById("d-tickspeedcost").value).toString();
+  }
+  if (updatedData.tickspeedMultiplier !== undefined) {
+    updatedData.tickspeedMultiplier = new Decimal(document.getElementById("d-tickspeedmultiplier").value).toString();
+  }
+  if (updatedData.tickSpeedMultDecrease !== undefined) {
+    updatedData.tickSpeedMultDecrease = new Decimal(document.getElementById("d-tickspeedmultdecrease").value).toString();
+  }
+  if (updatedData.tickSpeedMultDecreaseCost !== undefined) {
+    updatedData.tickSpeedMultDecreaseCost = new Decimal(document.getElementById("d-tickspeedmultdecreasecost").value).toString();
   }
 
   var encoded = btoa(JSON.stringify(updatedData));
